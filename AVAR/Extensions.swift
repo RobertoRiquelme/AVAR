e6908a1324f69637bbe919a77ded3bcdafb72bb8
Roberto Riquelme <roberto.riquelme@gmail.com>
2025-04-22 12:22:53 -0400

    Initial version, objects appearing on table surface

---
+++ AVAR/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  AVAR
+//
+//  Created by Roberto Riquelme on 22-04-25.
+//
+
+import Foundation
---
+++ AVAR/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  AVAR
+//
+//  Created by Roberto Riquelme on 22-04-25.
+//
+
+import Foundation
